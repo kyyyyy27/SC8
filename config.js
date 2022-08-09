07297b7215f31e60068026830e3dac2717367167
kyyyyy27 <107806866+kyyyyy27@users.noreply.github.com>
2022-08-10 01:06:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Zihroo'
+global.wm = 'Zhiroo'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
